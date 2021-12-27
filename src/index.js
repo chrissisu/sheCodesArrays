3ab6557b30570e6bf40e03f9150a1545440b7978
Chrissi <89806552+chrissisu@users.noreply.github.com>
2021-12-27 18:22:17 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+let weather = {
+  name: "weather"
+};
+console.log(weather);
+
+weather = {
+  name: "weather",
+  temp: 18,
+  humidity: 9
+};
+console.log(weather.temp);
+console.log(weather.humidity);
+
+console.log(weather);
+
+weather = [
+  {
+    name: "forecastMon",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  },
+
+  {
+    name: "forecastTu",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  },
+
+  {
+    name: "forecastWed",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  },
+
+  {
+    name: "forecastThu",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  },
+
+  {
+    name: "forecastFr",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  },
+
+  {
+    name: "forecastSat",
+    temp: 18,
+    humidity: 9,
+    windSpeed: 70
+  }
+];
+
+console.log(weather);
